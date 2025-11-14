a6af892c4b02ec2b9cc7a1f213c1694b7736002d
Nawamin ICE <157040537+Nanomete@users.noreply.github.com>
2025-11-14 21:01:55 +0700

    install lucide

--- src/components/people/PersonalList.jsx
+++ src/components/people/PersonalList.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
-import useDutyStore from '../../store/useDutyStore'
+import React from "react";
+import useDutyStore from "../../store/useDutyStore";
+import { Clock, User } from "lucide-react";
 
 const PersonalList = () => {
   // zustand store
-  const personnel = useDutyStore((state)=> {
-    return state.personnel
+  const personnel = useDutyStore((state) => {
+    return state.personnel;
   });
   console.log(personnel);
   return (
-    <div className='w-80 bg-gray-200 overflow-y-auto'>
-      Personal
-    </div>
-  )
-}
+    <div className="w-80 bg-white overflow-y-auto">
+      {/* text label header of personal list */}
+      <div className="p-6 border-b border-gray-400">
+        <div className="flex item-canter gap-3 mb-2">
+          {/* icon */}
+          <User className="text-blue-500" size={28} />
+          {/* text */}
+          <h2 className="text-2xl font-bold text-gray-800">
+            รายชื่อเจ้าหน้าที่
+          </h2>
+        </div>
+        {/* text */}
+        <p className="text-sm text-gray-500">ลากไปยังจุด</p>
+      </div>
 
-export default PersonalList
+      {/* item list of personnel list 
+      card of list*/}
+      <div className="p-2 space-y-1">
+        {personnel.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="flex items-center gap-3 p-3 bg-blue-100 border border-blue-300
+        rounded-lg cursor-move hover:shadow-md hover:scale-105"
+            >
+              <div className="text-3xl">{item.avatar}</div>
+              <div className="flex-1">
+                <div className="font-semibold text-gray-800">{item.name}</div>
+                <div className="text-xs text-gray-500">{item.position}</div>
+              </div>
+              <div className="text-gray-400">
+                <Clock />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default PersonalList;

1edcd6c35256ac976c9c5dc4b39383c3918ed934
Nawamin ICE <157040537+Nanomete@users.noreply.github.com>
2025-11-15 21:42:57 +0700

    install react-leaflet

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { MapContainer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import Layers from "./Layers";
 
 const MapView = () => {
+  const centerSetDefault = [14, 100];
   return (
-    <div className='flex-1 bg-green-300'>
-      MapView
+    <div className="flex-1 bg-green-300">
+      <MapContainer
+        className="h-full"
+        center={centerSetDefault}
+        zoom={13}
+        // scrollWheelZoom is to disable zooming with mouse wheel
+        scrollWheelZoom={true}
+      >
+        <Layers />
+      </MapContainer>
     </div>
-  )
-}
+  );
+};
 
-export default MapView
+export default MapView;

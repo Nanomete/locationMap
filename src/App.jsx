73427195e95da03018baadd5fdbe20d6bf2ed42d
Nawamin ICE <157040537+Nanomete@users.noreply.github.com>
2025-11-18 21:10:01 +0700

    add event of map in Mapview.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import PersonalList from './components/people/PersonalList'
-import Header from './components/layout/Header'
-import MapView from './components/map/MapView'
-import LocationList from './components/locations/LocationList'
+import React, { useEffect, useState } from "react";
+import PersonalList from "./components/people/PersonalList";
+import Header from "./components/layout/Header";
+import MapView from "./components/map/MapView";
+import LocationList from "./components/locations/LocationList";
 // import axios from 'axios'
-import useDutyStore from './store/useDutyStore'
+import useDutyStore from "./store/useDutyStore";
 
 function App() {
   // js
-  const [adding, setAdding] = useState(false)
+  // useState
+  const [adding, setAdding] = useState(false);
+  const [pending, setPending] = useState(null);
 
   // นำเข้ามาเพื่อโหลดข้อมูลให้ component ลูกแสดงข้อมูลจาก API ได้
-  const fetchAll = useDutyStore((state)=> {
-    return state.fetchAll
-  })
-  useEffect(()=> {
+  const fetchAll = useDutyStore((state) => {
+    return state.fetchAll;
+  });
+  useEffect(() => {
     // fn body
     fetchAll();
-  },[])
+  }, []);
+
+  const onPick = (lat, lng) => {
+    setPending({
+      lat: lat,
+      lng: lng,
+    });
+  };
+  console.log(pending);
 
   // html
   return (
-    <div className='flex h-screen bg-gray-300'>
+    <div className="flex h-screen bg-gray-300">
       <PersonalList />
-      <div className='flex flex-col flex-1'>
-        <Header 
-        adding={adding} setAdding={setAdding}
-        />
+      <div className="flex flex-col flex-1">
+        <Header adding={adding} setAdding={setAdding} />
 
-        <div className='flex flex-1 overflow-hidden'>
-          <MapView />
+        <div className="flex flex-1 overflow-hidden">
+          <MapView adding={adding} onPick={onPick} />
           <LocationList />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
